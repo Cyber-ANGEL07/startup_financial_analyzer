092e9b7f8e693df883911f67c2d7aef416e3e79c
Puvanica <puvanicak@gmail.com>
2026-05-22 13:51:26 +0530

    Seperate home screen from main app file

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Risk Analyzer'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to Startup Financial Trends and Risk Analyzer',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
