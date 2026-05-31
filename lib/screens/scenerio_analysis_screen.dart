69f8b13cc565fa948b6859afd475c4aa382c497c
Puvanica <puvanicak@gmail.com>
2026-05-31 21:45:52 +0530

    Add Hive packages for local data storage

--- lib/screens/scenerio_analysis_screen.dart
+++ lib/screens/scenerio_analysis_screen.dart
@@ -57,8 +57,37 @@ class _ScenarioAnalysisScreenState extends State<ScenarioAnalysisScreen> {
               child: const Text('Run Scenario'),
             ),
             const SizedBox(height: 24),
-            Text('New Burn Rate: LKR ${newBurnRate.toStringAsFixed(2)}'),
-            Text('New Runway: ${newRunway.toStringAsFixed(1)} months'),
+            Card(
+              child: Padding(padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Scenerio Result',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 12),
+                  Text(
+                    'Current Burn Rate: LKR ${FinancialData.burnRate.toStringAsFixed(2)}',                    
+                  ),
+                  Text(
+                    'New Burn Rate: LKR ${newBurnRate.toStringAsFixed(2)}'
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Current Runway: ${FinancialData.cashRunway.toStringAsFixed(1)} months',
+                  ),
+                  Text(
+                    'New Runway: ${newRunway.toStringAsFixed(1)} months',
+                  ),
+                ],
+              ),
+              ),
+            )
           ],
         ),
       ),
